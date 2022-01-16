b59c22d7e2179b12a7b23fac26d13175026ef682
GMat915 <97854407+GMat915@users.noreply.github.com>
2022-01-16 17:54:02 -0500

    change to millisecond input

--- custom.ts
+++ custom.ts
@@ -8,6 +8,8 @@ namespace animations {
      * @param ms How Many Milliseconds In Between Growing Stages
      */
     //% block="grow with $ms milliseconds between growing stages"
+    //% ms.min=1 x.max=10000
+    //% ms.fieldOptions.precision=1
     export function grow(ms:number): void {
         basic.showLeds(`
         # . . . .
